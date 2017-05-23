f51b8566d77b73f87a9ed54281da1cc07241f0aa
Frank Lauterwald <frank.lauterwald@noris.de>
2017-05-23 10:17:50 +0200

    Complete message deserialization test

--- src/test/java/de/noris/gocd/notification/hipchat/PluginSettingsTest.java
+++ src/test/java/de/noris/gocd/notification/hipchat/PluginSettingsTest.java
@@ -25,11 +25,31 @@ public class PluginSettingsTest {
     @Test
     public void shouldDeserializeFromJSON() throws Exception {
         PluginSettings pluginSettings = PluginSettings.fromJSON("{" +
-                "\"hipchat_room\": \"p@ssw0rd\", " +
-                "\"hipchat_api_key\": \"https://cloud.example.com/api/v1\" " +
+                "\"hipchat_server_url\": \"http://foo.bar/hipchat\", " +
+                "\"hipchat_token\": \"s3cr3t\", " +
+                "\"hipchat_room\": \"aRoom\", " +
+                "\"notify_start\": \"true\", " +
+                "\"notify_success\": \"true\", " +
+                "\"notify_failure\": \"false\", " +
+                "\"message_start\": \"started\", " +
+                "\"message_success\": \"passed\", " +
+                "\"message_failure\": \"failed\", " +
+                "\"color_start\": \"gray\", " +
+                "\"color_success\": \"green\", " +
+                "\"color_failure\": \"red\" " +
                 "}");
 
-        assertThat(pluginSettings.getRoom(), is("p@ssw0rd"));
-        assertThat(pluginSettings.getToken(), is("https://cloud.example.com/api/v1"));
+        assertThat(pluginSettings.getServerUrl(), is("http://foo.bar/hipchat"));
+        assertThat(pluginSettings.getRoom(), is("aRoom"));
+        assertThat(pluginSettings.getToken(), is("s3cr3t"));
+        assertThat(pluginSettings.isNotifyStart(), is(true));
+        assertThat(pluginSettings.isNotifySuccess(), is(true));
+        assertThat(pluginSettings.isNotifyFailure(), is(false));
+        assertThat(pluginSettings.getMessageStart(), is("started"));
+        assertThat(pluginSettings.getMessageSuccess(), is("passed"));
+        assertThat(pluginSettings.getMessageFailure(), is("failed"));
+        assertThat(pluginSettings.getColorStart(), is("gray"));
+        assertThat(pluginSettings.getColorSuccess(), is("green"));
+        assertThat(pluginSettings.getColorFailure(), is("red"));
     }
 }
